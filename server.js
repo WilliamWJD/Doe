c28a1ab72ad0b1db1a0c217a9d10502a47b2eea4
williamwjd <william.jose.wjd@gmail.com>
2020-02-18 11:25:52 -0300

    Organizando arquivos estáticos na pasta public

--- server.js
+++ server.js
@@ -1,8 +1,21 @@
 const express = require('express')
+const nunjucks=require('nunjucks')
 
 const server=express()
 
+// CONFIGURAR O SERVIDOR PARA APRESENTAR ARQUIVOS ESTÁTICOS
+server.use(express.static('public'))
+
+// CONFIGURANDO A TEMPLATE ENGINE
+nunjucks.configure("./",{
+    express : server
+})
+
 const port=3333
+
+server.get('/',function(req,res){
+    return res.render("index.html")
+})
 
 server.listen(port,()=>{
     console.log(`Servidor online na porta ${port}`)
