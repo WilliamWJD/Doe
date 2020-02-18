965d8ba015b996a3e00c77cd09451d6d9516e55d
williamwjd <william.jose.wjd@gmail.com>
2020-02-18 13:36:37 -0300

    Finalizando o backend da aplicação

--- server.js
+++ server.js
@@ -1,64 +1,58 @@
 const express = require('express')
-const nunjucks=require('nunjucks')
+const nunjucks = require('nunjucks')
 const Pool = require('pg').Pool
 
-const dbConfig=require('./config/database')
+const dbConfig = require('./config/database')
 
-const server=express()
+const server = express()
 
 // CONFIGURAR O SERVIDOR PARA APRESENTAR ARQUIVOS ESTÁTICOS
 server.use(express.static('public'))
 
 // HABILITAR BODY DO FORM
-server.use(express.urlencoded({ extended:true }))
+server.use(express.urlencoded({ extended: true }))
 
 // CONEXÃO COM BANCO DE DADOS
 const db = new Pool(dbConfig)
 
 // CONFIGURANDO A TEMPLATE ENGINE
-nunjucks.configure("./",{
-    express : server,
-    noCache : true
+nunjucks.configure("./", {
+    express: server,
+    noCache: true
 })
 
-const donors=[
-    {
-        name:"William José Dias",
-        blood:"AB+"
-    },
-    {
-        name:"Karolaine Ribeiro Dias",
-        blood:"A+"
-    },
-    {
-        name:"Luciana Pezarini",
-        blood:"O+"
-    },
-    {
-        name:"Gustavo Pezarini",
-        blood:"A-"
-    },
-]
+const port = 3333
 
-const port=3333
-
-server.get('/',function(req,res){
-    return res.render("index.html", {donors})
+server.get('/', function (req, res) {
+    db.query("SELECT * FROM donors", function (err, result) {
+        if (err) {
+            return res.send("Erro no banco de dados")
+        }
+        const donors=result.rows
+        return res.render("index.html", { donors })
+    })
 })
 
-server.post('/', function(req,res){
+server.post('/', function (req, res) {
     const name = req.body.name
     const email = req.body.email
     const blood = req.body.blood
 
-    donors.push({
-        name:name,
-        blood:blood
-    })
+    if (name == "" || email == "" || blood == "") {
+        return res.send("Todos os campos são obrigatórios.")
+    }
 
-    return res.redirect("/")
+    const query = `INSERT INTO donors ("name", "email", "blood") VALUES ($1, $2, $3)`
+    const values = [name, email, blood]
+
+    db.query(query, values, function (err) {
+        if (err) {
+            return res.send("Erro no banco de dados.")
+        }
+        return res.redirect("/")
+    })
 })
 
-server.listen(port,()=>{
+server.listen(port, () => {
     console.log(`Servidor online na porta ${port}`)
 })
